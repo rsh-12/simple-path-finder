29158fdf9b0c560cb26d23261541711cf08d7404
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-11 18:28:52 +0500

    Add Node class

---
+++ src/main/java/org/example/shortestpath/Node.java
@@ -0,0 +1,49 @@
+package org.example.shortestpath;
+
+class Node {
+    private final int row;
+    private final int column;
+
+    public Node(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node node = (Node) o;
+
+        if (row != node.row) return false;
+        return column == node.column;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + column;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "row=" + row +
+                ", column=" + column +
+                '}';
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getCost() {
+        return 1;
+    }
+}
