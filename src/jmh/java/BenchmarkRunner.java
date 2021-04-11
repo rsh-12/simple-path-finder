dcd599d6e31515d168c4142e6c60339da850ac91
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-04-11 21:22:44 +0500

    Make jmh tests runnable

--- src/jmh/java/BenchmarkRunner.java
+++ src/jmh/java/BenchmarkRunner.java
@@ -4,5 +4,11 @@
  * Time: 6:34 PM
  * */
 
+import java.io.IOException;
+
 public class BenchmarkRunner {
+
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
 }
